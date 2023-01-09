ea1c8fd88a1477b0c9891f465e71a50e66e6a6de
Ernest <99025962+EaleksS@users.noreply.github.com>
2023-01-09 20:50:36 +0900

    adaptiv

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import styles from './Nav.module.scss';
+import { GiHamburgerMenu } from 'react-icons/gi';
 
 const Nav = () => {
   const [scroll, setScroll] = useState(0);
+  const [openMenu, setOpenMenu] = useState(false);
 
   useEffect(() => {
     window.addEventListener('scroll', handleScroll);
@@ -15,7 +17,11 @@ const Nav = () => {
 
   return (
     <nav className={`${styles.nav} ${scroll && styles.active}`}>
-      <div className={`container ${styles.links} ${scroll && styles.linksActive}`}>
+      <div
+        className={`container ${styles.links}  ${
+          scroll && styles.linksActive
+        } ${styles.activeWeb}`}
+      >
         <a href="#hello">HELLO</a>
         <a href="#resume">RESUME</a>
         <a href="#portfolio">PORTFOLIO</a>
@@ -23,6 +29,46 @@ const Nav = () => {
         <a href="#blog">BLOG</a>
         <a href="#contact">CONTACT</a>
       </div>
+      {/* Burger menu */}
+      <div
+        className={`container ${styles.links}  ${
+          scroll && styles.linksActive
+        } ${styles.activeTable}`}
+      >
+        <span onClick={() => setOpenMenu(true)}>
+          <GiHamburgerMenu />
+        </span>
+      </div>
+      {/* Modal menu */}
+      {openMenu && (
+        <div className={`container`}>
+          <div className={`${styles.loyaut}`}>
+            <span onClick={() => setOpenMenu(false)}>x</span>
+          </div>
+          <div className={`${styles.menu}`}>
+            <div>
+              <a href="#hello" onClick={() => setOpenMenu(false)}>
+                HELLO
+              </a>
+              <a href="#resume" onClick={() => setOpenMenu(false)}>
+                RESUME
+              </a>
+              <a href="#portfolio" onClick={() => setOpenMenu(false)}>
+                PORTFOLIO
+              </a>
+              <a href="#testimonials" onClick={() => setOpenMenu(false)}>
+                TESTIMONIALS
+              </a>
+              <a href="#blog" onClick={() => setOpenMenu(false)}>
+                BLOG
+              </a>
+              <a href="#contact" onClick={() => setOpenMenu(false)}>
+                CONTACT
+              </a>
+            </div>
+          </div>
+        </div>
+      )}
     </nav>
   );
 };
