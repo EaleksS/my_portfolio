75438abd818482381922ab439350fba4483d0440
Ernest <99025962+EaleksS@users.noreply.github.com>
2023-01-10 16:15:33 +0900

    v3

--- src/components/Portfolio/Projects.tsx
+++ src/components/Portfolio/Projects.tsx
@@ -47,6 +47,39 @@ const projectsData: IProjectsData[] = [
     links: 'https://github.com',
     info: '#verstka',
   },
+  {
+    pid: 4,
+    image:
+      'https://previews.aspirity.com/spirit/assets/img/img_project_1_mono.png',
+    name: 'Мобильное и настольное приложение для магазина хостелов в Лондоне',
+    title: '---',
+    comment: '---',
+    stack: ['html5', 'css3', 'JavaScript', 'bower'],
+    links: 'https://github.com',
+    info: '#react',
+  },
+  {
+    pid: 5,
+    image:
+      'https://previews.aspirity.com/spirit/assets/img/img_project_2_mono.png',
+    name: 'СТРАНИЦА ВЕБ-ПРИЛОЖЕНИЯ ДЛЯ КОМПАНИИ TREVEL',
+    title: '---',
+    comment: '---',
+    stack: ['html5', 'css3', 'JavaScript', 'BEM', 'grunt'],
+    links: 'https://github.com',
+    info: '#wordpress',
+  },
+  {
+    pid: 6,
+    image:
+      'https://previews.aspirity.com/spirit/assets/img/img_project_3_mono.png',
+    name: 'ШАБЛОН АДМИНИСТРАТОРА ДЛЯ ФОТОСЕРВИСА',
+    title: '---',
+    comment: '---',
+    stack: ['html5', 'css3', 'JavaScript', 'Gulp', 'bower', 'grunt'],
+    links: 'https://github.com',
+    info: '#verstka',
+  },
 ];
 
 const Projects = ({ activeBtn }: any): JSX.Element => {
